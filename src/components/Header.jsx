6032a945689f351a3b81cb08549ed00ff05f81ca
Gabriel Alves <nfe@savantiplasticos.com.br>
2025-11-12 10:46:27 -0300

    Chris

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,8 +4,8 @@ export default function Header() {
   return (
     <header className="header">
       <div className="header-inner">
-        {/* bolinha com gradiente e iniciais (HRIS) */}
-        <div className="brand-badge">HRIS</div>
+        {/* bolinha com gradiente e iniciais (CHRIS) */}
+        <div className="brand-badge">CHRIS</div>
         <div>
           <div className="header-title">New Hydra</div>
           <div className="header-sub">Agenda • Financeiro • Clientes</div>
